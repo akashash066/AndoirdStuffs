467e4900a95c80183daf2d5cae6dd25e29951d25
akashash066 <akashbasnet066@gmail.com>
2024-06-08 16:34:50 +0530

    Add files via upload

---
+++ main/java/com/example/myapplication/MgMarg.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MgMarg extends AppCompatActivity {
+
+    private WebView mgmarg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mg_marg);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mgmarg=findViewById(R.id.mgmarg);
+        mgmarg.loadData(getString(R.string.mgmarg),"text/html","utf-8");
+    }
+}
